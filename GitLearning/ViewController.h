fe4d0db048a0ba11e26fb320f6dd460a67e6bc8c
shypusik <shypusik@hotmail.com>
2014-10-07 13:06:01 +0300

    first commit

---
+++ GitLearning/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitLearning
+//
+//  Created by Admin on 07.10.14.
+//  Copyright (c) 2014 LembergSun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
